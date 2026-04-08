e934718051459d1fc16f7a7b1c78475dfed3d4f8
ColinskiHD <colin@ketzberg.de>
2026-04-08 17:36:25 +0200

    added coin collection without score update

---
+++ DiscRunner/Assets/Scripts/Coin.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int coinValue = 1;
+
+    private void onTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            ScoreManager.instance.ChangeScore(coinValue);
+        }
+    }
+}
---
+++ DiscRunner/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    public TextMeshProUGUI text;
+    int score;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    public void ChangeScore(int coinValue)
+    {
+        score += coinValue;
+        text.text = "Score:" + score.ToString();
+    }
+}
