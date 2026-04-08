0a885ef75c0b913fb36b8a99a58187d2d3c41c4d
jeremy <quz31kep@uni-duesseldorf.de>
2026-04-08 12:44:51 +0200

    add a script for spinning an object

---
+++ DiscRunner/Assets/Scripts/SpinningMovement.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+
+    [SerializeField] private float rotationSpeed = 30f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(Vector3.forward, -rotationSpeed * Time.deltaTime);
+    }
+}
