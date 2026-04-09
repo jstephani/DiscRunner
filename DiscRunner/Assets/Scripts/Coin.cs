743fe4e2128868ecdc344d7b071b3361a143acf3
ColinskiHD <colin@ketzberg.de>
2026-04-09 22:37:41 +0200

    added SFX

--- DiscRunner/Assets/Scripts/Coin.cs
+++ DiscRunner/Assets/Scripts/Coin.cs
@@ -4,12 +4,19 @@ public class Coin : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public int coinValue = 1;
+    AudioManager audioManager;
+
+    public void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
             ScoreManager.instance.ChangeScore(coinValue);
+            audioManager.PlaySFX(audioManager.coin);
         }
     }
 }
