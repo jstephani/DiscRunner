94f4c404ca1303ad18af5bc593296ff7207d388f
ColinskiHD <colin@ketzberg.de>
2026-04-09 18:54:42 +0200

    added new background song

---
+++ DiscRunner/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("--------Audio Source--------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("--------Audio Clip--------")]
+    public AudioClip background;
+    public AudioClip death;
+    public AudioClip coin;
+    public AudioClip spike;
+
+    public void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+}
