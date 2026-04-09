35c94ea076c19dbb2e43d53c47b04b08b535b92c
ColinskiHD <colin@ketzberg.de>
2026-04-10 01:54:13 +0200

    changed score logic and added time to give score

--- DiscRunner/Assets/Scripts/ScoreManager.cs
+++ DiscRunner/Assets/Scripts/ScoreManager.cs
@@ -3,6 +3,11 @@ using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
+    [SerializeField] public float timeRate = 0.1f;
+    [SerializeField] public float multTimeRate = 10f;
+    private float multiplicator = 1f;
+    private float timer = 0f;
+    private float multTimer = 0f;
     public static ScoreManager instance;
     public TextMeshProUGUI text;
     int score;
@@ -14,11 +19,33 @@ public class ScoreManager : MonoBehaviour
             instance = this;
         }
     }
+    void Update()
+    {
+        if(timer < timeRate)
+        {
+            timer += Time.deltaTime;
+        }else
+        {
+            score += 1;
+            text.text = "Score:" + score.ToString();
+            timer = 0f;
+        }
+
+
+        if(multTimer < multTimeRate)
+        {
+            multTimer += Time.deltaTime;
+        }else
+        {
+            multiplicator += 0.1f;
+            multTimer = 0f;
+        }
+    }
 
     // Update is called once per frame
     public void ChangeScore(int coinValue)
     {
-        score += coinValue;
+        score += (int)(coinValue * multiplicator);
         text.text = "Score:" + score.ToString();
     }
     public int getScore()
